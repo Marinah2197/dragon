b511ada2d8a715b3777caf53ffbd6649c691a4c1
Marinah2197 <razrinah.m@gmail.com>
2022-10-08 11:42:32 +0300

    ''[FIX]

--- dragon_base/__manifest__.py
+++ dragon_base/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "dragon_base",
+    'name': "DRAGON TEST",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        la description de mon module""",
 
     'description': """
-        Long description of module's purpose
+        Description longue de mon module
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Kasia",
+    'website': "http://kasia.mg",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
